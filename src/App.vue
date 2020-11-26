8adf5e6826d6cc2abfb744609bdcd937bc1905cc
Erostin Lushka <erostin@Erostins-MacBook-Pro.local>
2020-11-26 00:56:45 -0500

    Add repo files

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app" class="container">
+    <SearchBar @termChange="onTermChange" />
+    <VideoDetail :video="selectedVideo" />
+    <VideoList @videoSelect="onVideoSelect" :videos="videos" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+
+export default {
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data() {
+    return {
+      videos: [],
+      selectedVideo: null
+    };
+  },
+  computed: {
+    getApiKey() {
+      return process.env.VUE_APP_API_KEY;
+    }
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: this.getApiKey,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => (this.videos = response.data.items));
+    }
+  }
+};
+</script>
+
+<style></style>
